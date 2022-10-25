ba0c1c2837148d40b6cfb6c9dcb6ab3814dc689b
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-10-25 12:29:52 -0500

    completed second fxn

--- gitprac.js
+++ gitprac.js
@@ -1,8 +1,17 @@
-//fxn 1
-
+//fxn 1 log 1 - 20
 const logNumbers = () => {
     for(let i = 1; i <= 20; i++) {
         console.log(i);
     }
 }
-logNumbers()
+logNumbers();
+
+//fxn 2 Get Even
+const getEven = () => {
+    for(let i = 1; i <= 200; i++) {
+        if(i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+getEven();
